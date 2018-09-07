fbf1bcd64b85c5c663174032e56e829b30b95795
Finem50 <Finem50@live.com>
2018-09-07 14:30:52 -0500

    Copy and pasted code from source. Not sure what the difference is but it works (but looks identical???)

--- ex61.cpp
+++ ex61.cpp
@@ -1,48 +1,47 @@
-//This program creates uses a structure for loan, initialize it from the keyboard
-//then, displays the struct
+// P61a.cpp - This program creates uses a structure for loan, initialize it from the keyboard, then
+// displays the struct
 
-#include <iostream>
-#include <cmath>
-
+#include<iostream>
+#include<cmath>
 using namespace std;
 
-struct Loan{ //Loan is the structure tag
-
-  int ID; //Assume a unique integer between 1111 - 9999
-  float amount; //$ amount of the loan
-  float rate; //Annual interest rate
-  int term; //Number of months, length of the loan
+struct Loan  // Loan is called structure tag
+{
+    int ID;  // assume an unique integer between 1111-9999
+    float amount; // $ amount of the loan
+    float rate; // annual interest rate
+    int term;  // number of months, length of the loan
 };
 
 float payment(Loan l);
 
-int main(){
+int main( )
+{
+      Loan loan1;
+      float monthly_payment;
 
-  Loan loan1;
-  float monthly_payment;
+      // Initialize the loan1 structure
+      cout << "Enter the ID of this loan \n";
+      cin >> loan1.ID;
 
-  //initialize the loan1 structure
-  cout << "Enter the ID of this loan \n";
-  cout >> loan1.ID;
+      cout << "Enter the amount of this loan \n";
+      cin >> loan1.amount;
 
-  cout << "Enter the amount of this loan \n";
-  cin >> loan1.amount;
+      cout << "Enter the annual interest rate of this loan (in %) \n";
+      cin >> loan1.rate;
 
-  cout << "Enter the annual interest rate of this loan (in %) \n";
-  cin >> loan1.rate;
+      cout << "Enter the term (number of months, length of the loan) \n";
+      cin >> loan1.term;
 
-  cout << "Enter the term (number of months), length of the loan) \n";
-  cin >> loan1.term;
+      monthly_payment  = payment(loan1);
 
-  monthly_payment = payment(loan1);
+      cout << "The monthly payment for loan " << loan1.ID << " is: " << monthly_payment << endl;
 
-  cout << "The monthly payment for loan " << loan1.ID << " is: " << monthly_payment << endl;
-
-  return 0;
+      return 0;
 }
 
-float payment(Loan l){
-
-  l.rate/1200; //To convert % yearly rate to monthly fraction
-  return l.amount * l.rate(pow((l.rate + 1), l.term)/(pow((l.rate + 1), l.term) - 1));
+float payment(Loan l)
+{
+      l.rate = l.rate/1200;  // To convert % yearly rate to monthly fraction
+      return l.amount*l.rate*(  pow( (l.rate+1), l.term)/ (pow( (l.rate+1), l.term) - 1) );
 }
